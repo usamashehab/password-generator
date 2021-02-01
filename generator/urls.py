5dc38aca959943346e7e9b8f656ec0ac78a181b8
usama shehab <usama.shehab173@gmail.com>
2021-02-01 20:52:29 +0200

    password generator with bootstrab

---
+++ generator/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('password/', views.password, name='password'),
+    path('about/', views.about, name='about')
+]
---
+++ generator/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+dect = {
+    'password': 'dfa2312bf2@',
+}
+
+
+def home(request):
+    return render(request, 'generator/home.html', context=dect)
+# def generate(request):
+
+
+def password(request):
+    character = list('qwertyuioplkjhgfdsazxcvbnm')
+    thepassword = ""
+    length = int(request.GET.get('length'))
+    if request.GET.get('uppercase'):
+        character.extend(list('QWERTYUIOPLKJHGFDSAZXCVBNM'))
+    if request.GET.get('numbers'):
+        character.extend(list('0123456789'))
+    if request.GET.get('special'):
+        character.extend(list('!@#$%^&*()_+='))
+    for itary in range(length):
+        thepassword += random.choice(character)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
